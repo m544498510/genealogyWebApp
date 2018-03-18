e5d06ff05733fd0715cc7a0b39933fe211695193
firefly_may <m544498510@gmail.com>
2018-03-18 22:15:35 +0800

    add client query code

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,9 +1,23 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import {render} from 'react-dom';
+import {ApolloClient} from 'apollo-client';
+import {HttpLink} from 'apollo-link-http';
+import {InMemoryCache} from 'apollo-cache-inmemory';
 
-import { Hello } from './test';
+import {ApolloProvider} from 'react-apollo';
+import Test from './test';
+import './test.css';
 
-ReactDOM.render(
-  <Hello compiler="TypeScript" framework="React" />,
-  document.getElementById('example')
+const client = new ApolloClient({
+  link: new HttpLink(),
+  cache: new InMemoryCache()
+});
+
+render(
+  (
+    <ApolloProvider client={client}>
+      <Test />
+    </ApolloProvider>
+  ),
+  document.getElementById('root')
 );
--- frontend/src/test.tsx
+++ frontend/src/test.tsx
@@ -1,5 +1,50 @@
 import * as React from 'react';
+import gql from 'graphql-tag';
+import {graphql} from 'react-apollo';
 
-export type HelloProps = { compiler: string; framework: string; };
+export type Props = {
+  data: {
+    loading: boolean,
+    error: object,
+    userList: object[]
+  }
+};
 
-export const Hello = (props: HelloProps) => <h1>Hello from {props.compiler} and {props.framework}!</h1>;
+const GET_UserList = gql`
+  query {
+    userList {
+      _id,
+      name,
+      password
+    }
+  }
+`;
+
+const CreateUser = gql`
+  mutation {
+    addUser (){
+      
+    }
+  }
+`;
+
+export class Test extends React.Component<Props> {
+  render() {
+    const {loading, error, userList} = this.props.data;
+    if(loading){
+      return <div>Loading....</div>
+    }else if(error){
+      return <div>{JSON.stringify(error)}</div>
+    }else if(userList && userList.length > 0){
+      return userList.map(item => (
+        <div>
+          <span>Id: {item._id}</span>
+          <span style={{marginLeft: 20}}>Name: {item.name}</span>
+          <span style={{marginLeft: 20}}>Password: {item.password}</span>
+        </div>
+      ));
+    }
+  }
+}
+
+export default graphql(GET_UserList)(Test);
